b9261cafe98b6e346f1ee21a7ab9482033745847
daffa tabiano <daffatabianoo@gmail.com>
2024-04-03 01:13:21 +0700

    add header picture

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,10 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
 
-
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </Provider>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 // import usePost from '@/hooks/usePost';
 import AuthLayout from '@/Layout/AuthLayout';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import home from '@/styles/home.module.css';
 
 export default function Home() {
     return (
-        <AuthLayout className={` ${inter.className}`}>
-            <h1>ini home page </h1>
+        <AuthLayout>
+            <header className={home.header}>
+                <img src="/images/to-ravel-header.jpg" alt="logo" />
+                <div className={home['header-content']}>
+                    <h1>FINDING YOUR FREEDOM HERE !</h1>
+                    <p>Expolore The World with your Vehicle&apos;s dream</p>
+                </div>
+            </header>
         </AuthLayout>
-    );
+    )
 }
