705de1253471be399ce477871ffdebce02d51e60
daffa tabiano <daffatabianoo@gmail.com>
2024-04-05 10:49:25 +0700

    fixing layout of home-header

--- src/Layout/HomeLayout.tsx
+++ src/Layout/HomeLayout.tsx
@@ -7,16 +7,20 @@ export default function HomeLayout() {
             <header className={home.header}>
                 <img src="/images/to-ravel-header.jpg" alt="logo" />
                 <div className={home['header-content']}>
-                    <Button
-                        color={`${home['button-color']} ${home['button-width']}`}
-                    >
-                        Unlimited Explore !{' '}
-                        <a href="" className="text-blue-500 underline">
-                            Read More
-                        </a>
-                    </Button>
-                    <h1>FINDING YOUR FREEDOM HERE !</h1>
-                    <p>Expolore The World with your Vehicle&apos;s dream</p>
+                    <div>
+                        <Button
+                            color={`${home['button-color']} ${home['button-width']}`}
+                        >
+                            Unlimited Explore !{' '}
+                            <a href="" className="text-blue-500 underline">
+                                Read More
+                            </a>
+                        </Button>
+                    </div>
+                    <div>
+                        <h1>FINDING YOUR FREEDOM HERE !</h1>
+                        <p>Expolore The World with your Vehicle&apos;s dream</p>
+                    </div>
                     <div className="flex">
                         <Button color={home['button-color']}>Learn More</Button>
                         <Button>Get Started</Button>
