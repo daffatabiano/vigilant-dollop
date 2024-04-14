37513d172f8adb62bf4cd48396bb450a56c6f332
daffa tabiano <daffatabianoo@gmail.com>
2024-04-14 21:40:39 +0700

    make destination page and get data with ssr

--- src/fragments/sections/FAQ .tsx
+++ src/fragments/sections/FAQ .tsx
@@ -1,14 +1,67 @@
 import SupportingFacilites from '@/components/SupportingFacilities';
-import AccordionFAQ from '@/components/Accordion';
+import { Accordion, AccordionItem } from '@nextui-org/react';
+import { useState } from 'react';
 
 export default function FAQ() {
+    const [selectedKey, setSelectedKey] = useState(new Set(['1']));
     return (
-        <section className="mt-5">
+        <section className="mt-6 p-3 bg-secondary">
             <SupportingFacilites
                 title="Travel Freedom FAQs"
                 desc="Frequently Asked Questions about To Ravel Find Freedom"
             />
-            <AccordionFAQ />
+            <Accordion>
+                <AccordionItem
+                    key="1"
+                    aria-label="Accordion 1"
+                    title="Accordion 1"
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus at eveniet optio architecto necessitatibus! Deserunt
+                    voluptatum hic voluptates culpa, reprehenderit quisquam in
+                    ipsam facere aliquid iusto inventore iste numquam molestias.
+                </AccordionItem>
+                <AccordionItem
+                    key="2"
+                    aria-label="Accordion 2"
+                    title="Accordion 2"
+                >
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Asperiores veniam iste impedit repellendus! Tempore placeat
+                    earum molestias quam. Natus distinctio nostrum totam ipsum
+                    provident eveniet nemo nobis quibusdam magnam aliquam.
+                </AccordionItem>
+                <AccordionItem
+                    key="3"
+                    aria-label="Accordion 3"
+                    title="Accordion 3"
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus explicabo nemo consequuntur eaque autem perspiciatis.
+                    Inventore voluptates rem totam ipsam atque officia, possimus
+                    fugiat repellendus ducimus vitae neque quibusdam dolores.
+                </AccordionItem>
+                <AccordionItem
+                    key="4"
+                    aria-label="Accordion 4"
+                    title="Accordion 4"
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus explicabo nemo consequuntur eaque autem perspiciatis.
+                    Inventore voluptates rem totam ipsam atque officia, possimus
+                    fugiat repellendus ducimus vitae neque quibusdam dolores.
+                </AccordionItem>
+                <AccordionItem
+                    key="5"
+                    aria-label="Accordion 5"
+                    title="Accordion 5"
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus explicabo nemo consequuntur eaque autem perspiciatis.
+                    Inventore voluptates rem totam ipsam atque officia, possimus
+                    fugiat repellendus ducimus vitae neque quibusdam dolores.
+                </AccordionItem>
+            </Accordion>
         </section>
     );
 }
--- src/pages/destination/index.tsx
+++ src/pages/destination/index.tsx
@@ -1,13 +1,17 @@
-import { Carousel } from 'react-bootstrap';
+import { Button, Card, Carousel } from 'react-bootstrap';
 import Navbar from 'src/fragments/Navbar';
 import style from '@/styles/destination.module.css';
+import SupportingFacilites from 'src/components/SupportingFacilities';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 export async function getServerSideProps() {
+    const apikey = '24405e01-fbc1-45a5-9f5a-be13afcd757c';
     const resp = await fetch(
         'https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/banners',
         {
             headers: {
-                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                apiKey: apikey,
             },
         }
     );
@@ -15,21 +19,55 @@ export async function getServerSideProps() {
         'https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/promos',
         {
             headers: {
-                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                apiKey: apikey,
             },
         }
     );
+    const resCategory = await fetch(
+        'https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/categories',
+        {
+            headers: {
+                apiKey: apikey,
+            },
+        }
+    );
+    const resActivites = await fetch(
+        'https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/categories',
+        {
+            headers: {
+                apiKey: apikey,
+            },
+        }
+    );
+    const dataActivites = await resActivites.json();
+    const dataCategory = await resCategory.json();
     const dataPromo = await resPromo.json();
     const data = await resp.json();
-    console.log(dataPromo);
+    console.log(dataActivites.data);
     return {
-        props: { product: data.data, promo: dataPromo.data },
+        props: {
+            product: data.data,
+            promo: dataPromo.data,
+            category: dataCategory.data,
+            activities: dataActivites.data,
+        },
     };
 }
-export default function Destination(props: { product: any }) {
-    const { product } = props;
+export default function Destination(props: {
+    product: any;
+    promo: any;
+    category: any;
+    activities: any;
+}) {
+    const { product, promo, category, activities } = props;
+    const responsive = {
+        0: { items: 1 },
+        568: { items: 2 },
+        1024: { items: 3 },
+    };
+
     return (
-        <>
+        <div className="bg-secondary">
             <Navbar />
             <div className={`${style['banner-title']}`}>
                 <h1>
@@ -50,6 +88,102 @@ export default function Destination(props: { product: any }) {
                     </Carousel.Item>
                 ))}
             </Carousel>
-        </>
+            <SupportingFacilites
+                title="Catchy Discount"
+                desc="Don't Miss it and Run Out"
+            />
+
+            <div className="d-flex mt-6">
+                <div className="d-flex flex-column w-25 p-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="50"
+                        height="50"
+                        fill="currentColor"
+                        className="bi bi-ticket-detailed"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M5 7a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2z" />
+                        <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5z" />
+                    </svg>
+                    <h1 className="fs-1 fw-bold">Try to Our Promo</h1>
+                </div>
+                <div className="w-75">
+                    <AliceCarousel infinite responsive={responsive}>
+                        {promo.map((item: any) => (
+                            <div key={item.id}>
+                                <Card style={{ width: '18rem' }}>
+                                    <img
+                                        className="w-100 h-100"
+                                        variant="top"
+                                        src={item.imageUrl}
+                                    />
+                                    <Card.Body className={style['promo-cb']}>
+                                        <Card.Title>{item.title}</Card.Title>
+                                        <Card.Text className="text-black">
+                                            {item.description}
+                                        </Card.Text>
+                                        <Button variant="primary">
+                                            Go somewhere
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))}
+                    </AliceCarousel>
+                </div>
+            </div>
+            <div className="d-flex">
+                <div className="w-75">
+                    <AliceCarousel infinite responsive={responsive}>
+                        {category.map((item: any) => (
+                            <div key={item.id}>
+                                <Card style={{ width: '18rem' }}>
+                                    <img
+                                        className="w-100 h-100"
+                                        variant="top"
+                                        src={item.imageUrl}
+                                    />
+                                    <Card.Body className={style['promo-cb']}>
+                                        <Card.Title>{item.name}</Card.Title>
+                                        <Card.Text className="text-black">
+                                            {item.description}
+                                        </Card.Text>
+                                        <Button variant="primary">
+                                            Go somewhere
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))}
+                    </AliceCarousel>
+                </div>
+                <div className="d-flex flex-column w-25 p-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="50"
+                        height="50"
+                        fill="currentColor"
+                        className="bi bi-ticket-detailed"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M5 7a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2z" />
+                        <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5z" />
+                    </svg>
+                    <h1 className="fs-1 fw-bold">Try to Our Promo</h1>
+                </div>
+            </div>
+            <div className="mt-6">
+                <SupportingFacilites
+                    title="Package Destination"
+                    desc="destination package more than cheapers"
+                />
+            </div>
+            {activities.map((item: any) => (
+                <div key={item.id}>
+                    <img src={item.imageUrl} alt="" />
+                </div>
+            ))}
+        </div>
     );
 }
