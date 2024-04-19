1a6279d478b847d17396455e1f3d35d9df358c1c
daffa tabiano <daffatabianoo@gmail.com>
2024-04-20 03:01:01 +0700

    add activity section card

--- src/pages/destination/comp/Activity.tsx
+++ src/pages/destination/comp/Activity.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react';
+import ButtonSectionContainer from 'src/components/elements/Button/ButtonSectionContainer';
 import useGet from 'src/hooks/useGet';
+import style from '@/styles/activity.module.css';
+import { div } from 'react-bootstrap';
 
 export default function Activity() {
     const { getData, data } = useGet();
@@ -7,6 +10,41 @@ export default function Activity() {
     useEffect(() => {
         getData('activities');
     }, []);
-    console.log(data.data);
-    return <></>;
+
+    console.log(data);
+
+    return (
+        <div className={style['activity']}>
+            <h1>TOP BOOK NOW</h1>
+            <div className={style.card}>
+                {data?.map((item: any) => (
+                    <div key={item.id} className={style['fill-card']}>
+                        <div className={style['card-headers']}>
+                            <span className={style.badge}>
+                                ⭐ {item.rating}
+                            </span>
+                            <img src={item.imageUrls[1]} alt="maldives" />
+                        </div>
+                        <div className={style['card-content']}>
+                            <h6>
+                                {item.title} <span>📍 {item.province}</span>
+                            </h6>
+                            <p>
+                                {item.price.toLocaleString('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD',
+                                })}
+                                {/* <span>{item.discount}</span> */}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={style['button']}>
+                <ButtonSectionContainer className={style.button}>
+                    See All
+                </ButtonSectionContainer>
+            </div>
+        </div>
+    );
 }
--- src/pages/destination/index.tsx
+++ src/pages/destination/index.tsx
@@ -69,102 +69,11 @@ export default function Destination() {
                 alt="maldives"
                 className={style['banner-image']}
             />
-            {/* <Carousel indicators={false} controls={false}>
-                {product.map((item: any) => (
-                    <Carousel.Item interval={2000} key={item.id}>
-                        <img
-                            className={'w-100'}
-                            style={{ height: '50vh' }}
-                            src={item.imageUrl}
-                            alt={item.name}
-                        />
-                    </Carousel.Item>
-                ))}
-            </Carousel> */}
             <Discount />
 
             <Banner />
 
             <Activity />
-            {/* <div className="d-flex">
-                <div className="w-75">
-                    <AliceCarousel infinite responsive={responsive}>
-                        {category.map((item: any) => (
-                            <div key={item.id}>
-                                <Card style={{ width: '18rem' }}>
-                                    <img
-                                        className="w-100 h-100"
-                                        variant="top"
-                                        src={item.imageUrl}
-                                    />
-                                    <Card.Body className={style['promo-cb']}>
-                                        <Card.Title>{item.name}</Card.Title>
-                                        <Card.Text className="text-black">
-                                            {item.description}
-                                        </Card.Text>
-                                        <Button variant="primary">
-                                            Go somewhere
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        ))}
-                    </AliceCarousel>
-                </div>
-                <div className="d-flex flex-column w-25 p-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="50"
-                        height="50"
-                        fill="currentColor"
-                        className="bi bi-ticket-detailed"
-                        viewBox="0 0 16 16"
-                    >
-                        <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M5 7a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2z" />
-                        <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5z" />
-                    </svg>
-                    <h1 className="fs-1 fw-bold">Try to Our Promo</h1>
-                </div>
-            </div>
-            <div className="mt-6">
-                <SupportingFacilites
-                    title="TOP DESTINATION'S"
-                    desc="top selling"
-                />
-            </div>
-            <div className={style['container-activities']}>
-                {activities.map((item: any) => (
-                    <div key={item.id} className={style.card}>
-                        <div>
-                            <img
-                                src={item.imageUrls}
-                                alt={item.title}
-                                // className={`${style.card}`}
-                            />
-                        </div>
-                        <div className={`d-flex`}>
-                            <h6 className={`flex-column`}>
-                                {item.title}
-                                <span>
-                                    {item.city}, {item.province}
-                                </span>
-                            </h6>
-                            <p className="text-success">
-                                {item.price_discount.toLocaleString('id-ID', {
-                                    style: 'currency',
-                                    currency: 'IDR',
-                                })}
-                            </p>
-                            <p className="text-decoration-line-through text-danger">
-                                {item.price.toLocaleString('id-ID', {
-                                    style: 'currency',
-                                    currency: 'IDR',
-                                })}
-                            </p>
-                        </div>
-                    </div>
-                ))}
-            </div> */}
         </div>
     );
 }
